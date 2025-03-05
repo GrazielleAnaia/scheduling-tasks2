41e48c4965682054d05fe00ea4ed5ffd7768c9a5
Grazielle <grazielle.anaia@gmail.com>
2025-03-05 10:14:10 -0500

    initial commit

---
+++ src/main/java/com/grazielleanaia/scheduling_tasks2/SchedulingTasks2Application.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.scheduling_tasks2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchedulingTasks2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchedulingTasks2Application.class, args);
+	}
+
+}
